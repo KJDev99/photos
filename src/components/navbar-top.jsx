86378725d496ce9f1db061634f0637abce638dd1
kjdev99 <jamshidqayimov0399@gmail.com>
2024-05-26 05:32:03 +0500

    v-1

---
+++ src/components/navbar-top.jsx
@@ -0,0 +1,28 @@
+const NavbarTop = () => {
+  return (
+    <div className="container mx-auto max-md:hidden">
+      <div className="flex justify-center my-3">
+        <a className="flex mx-4">
+          <img src="./adress.svg" alt="email" />
+          <p className="ml-3 text-[14px] text-[rgba(0,0,0,0.6)]">
+            Abdulla qodiry ko’chasi, 12A uy
+          </p>
+        </a>
+        <a href="mailto:johndoe@gmail.com" className="flex mx-4">
+          <img src="./email.svg" alt="email" />
+          <p className="ml-3 text-[14px] text-[rgba(0,0,0,0.6)]">
+            johndoe@gmail.com
+          </p>
+        </a>
+        <a href="tel:+998911111111" className="flex mx-4">
+          <img src="./phone.svg" alt="email" />
+          <p className="ml-3 text-[14px] text-[rgba(0,0,0,0.6)]">
+            +998 11 111-11-11
+          </p>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarTop;
---
+++ src/pages/home.jsx
@@ -0,0 +1,25 @@
+import Cart from "../components/carts";
+import Footer from "../components/footer";
+import Galery from "../components/galery";
+import Header from "../components/header";
+import Hero from "../components/hero";
+import NavbarTop from "../components/navbar-top";
+import Opinion from "../components/opinion";
+import Step from "../components/step";
+
+const Home = () => {
+  return (
+    <div>
+      <NavbarTop />
+      <Header />
+      <Hero />
+      <Step />
+      <Cart />
+      <Galery />
+      <Opinion />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
