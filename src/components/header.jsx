4efac2e2e824b37c0235dc71e29baa51efe43627
kjdev99 <jamshidqayimov0399@gmail.com>
2024-05-28 09:11:27 +0500

    navbar-menu

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
 
 const Header = () => {
+  const [menuActive, setMenuActive] = useState(true);
   return (
     <header className="text-gray-600 body-font">
-      <div className="container mx-auto flex flex-wrapflex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <div className="container mx-auto flex flex-wrap md:flex-row items-center justify-between max-md:mt-4">
+        <Link
+          to={"/"}
+          className="flex title-font font-medium items-center text-gray-900 md:mb-0"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -15,14 +22,38 @@ const Header = () => {
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
           <span className="ml-3 text-xl">BRENT_NAME</span>
-        </a>
-        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <Link to={'/'} className="mr-5 hover:text-gray-900">Home</Link>
-          <Link to={'/katalog'} className="mr-5 hover:text-gray-900">Katalog</Link>
-          <Link to={'/about'} className="mr-5 hover:text-gray-900">About</Link>
-          <Link to={'/contact'} className="mr-5 hover:text-gray-900">Contact</Link>
+        </Link>
+        <nav
+          className={`max-md:mx-auto flex flex-wrap items-center text-base justify-center max-md:absolute max-md:flex-col max-md:left-1/2 max-md:translate-x-[-50%] max-md:bg-slate-800 max-md:w-4/5 py-5 rounded max-md:text-white max-md:z-50 ${
+            !menuActive ? "max-md:top-20" : "max-md:top-[-150px]"
+          }`}
+        >
+          <Link
+            to={"/"}
+            className="mr-5 max-md:hover:text-slate-500 hover:text-gray-900"
+          >
+            Home
+          </Link>
+          <Link
+            to={"/katalog"}
+            className="mr-5 max-md:hover:text-slate-500 hover:text-gray-900"
+          >
+            Katalog
+          </Link>
+          <Link
+            to={"/about"}
+            className="mr-5 max-md:hover:text-slate-500 hover:text-gray-900"
+          >
+            About
+          </Link>
+          <Link
+            to={"/contact"}
+            className="mr-5 max-md:hover:text-slate-500 hover:text-gray-900"
+          >
+            Contact
+          </Link>
         </nav>
-        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+        {/* <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base  md:mt-0">
           Button
           <svg
             fill="none"
@@ -32,7 +63,18 @@ const Header = () => {
           >
             <path d="M5 12h14M12 5l7 7-7 7"></path>
           </svg>
-        </button>
+        </button> */}
+        {menuActive ? (
+          <GiHamburgerMenu
+            onClick={() => setMenuActive(!menuActive)}
+            className="text-2xl md:hidden"
+          />
+        ) : (
+          <IoClose
+            onClick={() => setMenuActive(!menuActive)}
+            className="text-2xl md:hidden"
+          />
+        )}
       </div>
     </header>
   );
